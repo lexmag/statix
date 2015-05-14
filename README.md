35cb448cbfea49c1c41834ebcf4c1aca1577ced2
Aleksei Magusev <lexmag@me.com>
2015-05-14 22:35:49 +0200

    Publish to the Hex

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Statix [![Build Status](https://travis-ci.org/lexmag/statix.svg)](https://travis-ci.org/lexmag/statix)
 
+Statix is an Elixir client for StatsD compatible servers.
+
+## Installation
+
+Add Statix as a dependency to your `mix.exs` file:
+
+```elixir
+def application() do
+  [applications: [:statix]]
+end
+
+defp deps() do
+  [{:statix, "~> 0.0.1"}]
+end
+```
+
+Then run `mix deps.get` in your shell to fetch the dependencies.
+
 ## License
 
 This software is licensed under [the ISC license](LICENSE).
--- mix.exs
+++ mix.exs
@@ -4,10 +4,22 @@ defmodule Statix.Mixfile do
   def project() do
     [app: :statix,
      version: "0.0.1",
-     elixir: "~> 1.0"]
+     elixir: "~> 1.0",
+     description: description(),
+     package: package()]
   end
 
   def application() do
     [applications: []]
+  end
+
+  defp description() do
+    "An Elixir client for StatsD compatible servers."
+  end
+
+  defp package() do
+    [contributors: ["Aleksei Magusev"],
+     licenses: ["ISC"],
+     links: %{"GitHub" => "https://github.com/lexmag/statix"}]
   end
 end
