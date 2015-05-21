9ee863507eea7385178ce6c2643c32b8e17bd924
Aleksei Magusev <lexmag@me.com>
2015-05-21 23:24:46 +0200

    Release v0.0.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Statix.Mixfile do
 
   def project() do
     [app: :statix,
-     version: "0.0.2",
+     version: "0.0.3",
      elixir: "~> 1.0",
      description: description(),
      package: package()]
