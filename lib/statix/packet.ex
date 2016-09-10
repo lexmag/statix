991eac3dce071747cac4ba5087b677b7260d0e99
Aleksei Magusev <lexmag@me.com>
2016-09-11 01:51:21 +0200

    Support Erlang 19 inet_drv changes

--- lib/statix/packet.ex
+++ lib/statix/packet.ex
@@ -1,13 +1,18 @@
 defmodule Statix.Packet do
   use Bitwise
 
+  otp_release = :erlang.system_info(:otp_release)
+  @addr_family if(otp_release >= '19', do: [1], else: [])
+
   def header({n1, n2, n3, n4}, port) do
-    [band(bsr(port, 8), 0xFF),
-     band(port, 0xFF),
-     band(n1, 0xFF),
-     band(n2, 0xFF),
-     band(n3, 0xFF),
-     band(n4, 0xFF)]
+    @addr_family ++ [
+      band(bsr(port, 8), 0xFF),
+      band(port, 0xFF),
+      band(n1, 0xFF),
+      band(n2, 0xFF),
+      band(n3, 0xFF),
+      band(n4, 0xFF)
+    ]
   end
 
   def build(header, name, key, val) do
