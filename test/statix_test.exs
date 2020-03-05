2fef0f5ede3140f1fb3697e64e5503adfcdb2e16
Aleksei Magusev <248290+lexmag@users.noreply.github.com>
2020-03-05 01:08:56 +0100

    Introduce Statix struct to hold connection and global tags (#49)
    This should also make future addition of global sample rate and polling easier.

--- test/statix_test.exs
+++ test/statix_test.exs
@@ -8,7 +8,7 @@ defmodule StatixTest do
   use Statix, runtime_config: unquote(runtime_config?)
 
   defp close_port() do
-    %Statix.Conn{sock: sock} = current_conn()
+    %{conn: %Statix.Conn{sock: sock}} = current_statix()
     Port.close(sock)
   end
 
@@ -155,30 +155,6 @@ defmodule StatixTest do
     set("sample", 3, sample_rate: 0.0)
 
     refute_received _any
-  end
-
-  test "global tags when present" do
-    Application.put_env(:statix, :tags, ["tag:test"])
-
-    increment("sample", 3)
-    assert_receive {:test_server, "sample:3|c|#tag:test"}
-
-    increment("sample", 3, tags: ["foo"])
-    assert_receive {:test_server, "sample:3|c|#foo,tag:test"}
-  after
-    Application.delete_env(:statix, :tags)
-  end
-
-  test "global connection-specific tags" do
-    Application.put_env(:statix, __MODULE__, tags: ["tag:test"])
-
-    increment("sample", 3)
-    assert_receive {:test_server, "sample:3|c|#tag:test"}
-
-    increment("sample", 3, tags: ["foo"])
-    assert_receive {:test_server, "sample:3|c|#foo,tag:test"}
-  after
-    Application.delete_env(:statix, __MODULE__)
   end
 
   test "port closed" do
